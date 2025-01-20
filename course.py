c3704efd27be9475798e36cefbde09455f9f8b6f
Dashkere404 <145912572+Dashkere404@users.noreply.github.com>
2025-01-20 20:10:52 +0300

    Удаление строк, где есть хотя бы одно пустое значение, а также удаление дубликатов

--- course.py
+++ course.py
@@ -1,13 +1,15 @@
 import kagglehub
 import pandas as pd
-# Download latest version
+# Загрузка датасета
 path = kagglehub.dataset_download("kanametov/movies-recomendation-system")
 movies_df=pd.read_csv(f"{path}/movies.csv")
 links_df=pd.read_csv(f"{path}/links.csv")
 ratings_df=pd.read_csv(f"{path}/ratings.csv")
-tags_dfh=pd.read_csv(f"{path}/tags.csv")
-print ("path: ", path)
-print(movies_df["title"])
-print(links_df.head())
+tags_df=pd.read_csv(f"{path}/tags.csv")
 
+#Удаление дубликатов, где это необходимо, а также строк, которые имеют хотя бы одно пустое значение
+movies_df=movies_df.drop_duplicates(subset=['movieId']).dropna()
+links_df=links_df.drop_duplicates(subset=['movieId']).dropna()
+ratings_df=ratings_df.dropna()
+tags_df=tags_df.dropna()
 
